f6d14443dd1d21adf6c0c4e30d2409a534c73914
farooquememon385 <farooquememon385@gmail.com>
2024-06-28 16:32:10 +0500

    feat: added Home Page and Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
-function Footer()
-{
-    return(
-        <p>Footer</p>
-    )
-}
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Farooq</div>
+      <div className={styles.text}>
+        Farooq creative thoughts © All rights reserved.
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
